4b94616494c68ea5167b43510c74f2382f02ee66
mantrivipin <30387054+mantrivipin@users.noreply.github.com>
2019-03-13 17:48:13 +0530

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'pry'
 require 'coveralls'
 Coveralls.wear!
 
-UnsplashVipin.configure do |config|
+Unsplash.configure do |config|
   config.application_access_key = "baaa6a1214d50b3586bec6e06157aab859bd4d86dc0b755360f103f38974edc3"
   config.application_secret = "bb834160d12304045c55d0c0ec2eb0fe62a5fe249bc1a392386120d55eb2793a"
   config.utm_source = "unsplash_rb_specs"
